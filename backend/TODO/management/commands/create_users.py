202064a3b65868d2386c54fd20a7fc04b44f4c5c
Roman <rombos1978@gmail.com>
2022-08-07 11:34:43 +0300

    1_2

--- backend/TODO/management/commands/create_users.py
+++ backend/TODO/management/commands/create_users.py
@@ -1,19 +1,27 @@
 from django.core.management import BaseCommand
 from TODO.models import User
 from django.utils.crypto import get_random_string
+from randomuser import RandomUser
 
 
 class Command(BaseCommand):
-    # help = u'Создание случайного пользователя'
-    #
-    # def add_arguments(self, parser):
-    #     parser.add_argument('total', type=int, help=u'Количество создаваемых пользователей')
-    #
-    # def handle(self, *args, **kwargs): total = kwargs['total'] for i in range(total): User.objects.create_user(
-    # username=get_random_string(), first_name=get_random_string(), last_name=get_random_string())
-    def handle(self, *args, **options):
-        users_list = []
-        for i in range(1, 11):
-            users_list.append(User(username=get_random_string(), first_name=get_random_string(), last_name=get_random_string(), email=''))
+    def handle(self, *args, **kwargs):
+        users_list = RandomUser.generate_users(3)
+        email_list = ['1@gb.ru', '2@gb.ru', '3@gb.ru']
+        for x in email_list:
+            x = x
+            users_list.append(
+                User(username=get_random_string(), first_name=get_random_string(), last_name=get_random_string(), email=x))
 
-        User.objects.bulk_create(users_list)
+
+# class Command(BaseCommand):
+#     def handle(self, *args, **options):
+#         users_list = []
+#         email_list = ['1@gb.ru', '2@gb.ru', '3@gb.ru']
+#
+#         for i in range(1, 4):
+#             users_list.append(
+#                 User(username=get_random_string(), first_name=get_random_string(), last_name=get_random_string(),
+#                      email=x))
+#
+#         User.objects.bulk_create(users_list)
