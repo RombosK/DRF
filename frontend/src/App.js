f55606f12a14bd83921b3b0a330a062ed2881615
Roman <rombos1978@gmail.com>
2022-08-18 23:31:46 +0300

    5@2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,21 @@ import axios from 'axios'
 import UserList from './components/UserList.js'
 import MenuList from './components/Menu.js'
 import FooterList from './components/Footer.js'
-// import './index.css'
-// import './App.css'
+import {BrowserRouter, Route, Routes, Link, Navigate, useLocation} from 'react-router-dom'
+import ProjectList from './components/ProjectList'
+import TodoList from './components/TodoList'
+import UserProjectList from './components/UserProjectList'
 
+
+const NotFound = () => {
+    let {pathname} = useLocation()
+
+    return (
+        <div>
+            Page '{pathname}'     NOT FOUND. PLEASE, TRY AGAIN
+        </div>
+    )
+}
 
 class App extends React.Component {
     menu = [
@@ -46,7 +58,9 @@ class App extends React.Component {
         this.state = {
             'menu': this.menu,
             'footer': this.footer,
-            'users': []
+            'users': [],
+            'projects': [],
+            'todos': [],
         }
     }
 
@@ -54,10 +68,34 @@ class App extends React.Component {
         axios
             .get('http://127.0.0.1:8000/api/users/')
             .then(response => {
-                const users = response.data
+                const users = response.data.results
                 this.setState(
                     {
                         'users': users
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+
+        axios
+            .get('http://127.0.0.1:8000/api/projects/')
+            .then(response => {
+                const projects = response.data.results
+                this.setState(
+                    {
+                        'projects': projects
+                    }
+                )
+            })
+            .catch(error => console.log(error))
+
+        axios
+            .get('http://127.0.0.1:8000/api/todos/')
+            .then(response => {
+                const todos = response.data.results
+                this.setState(
+                    {
+                        'todos': todos
                     }
                 )
             })
@@ -66,18 +104,39 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className='App'>
                 {<MenuList item_menu={this.state.menu} />}
-                <div className='content'>
-                    {<UserList users={this.state.users} />}
-                </div>
+            <div>
+                <BrowserRouter>
+                    <nav>
+                        <li> <Link to='/'>Users</Link></li>
+                        <li> <Link to='/projects'>Projects</Link></li>
+                        <li> <Link to='/todos'>Todos</Link></li>
+                    </nav>
 
-                <div className='panel-footer'>
-                    {<FooterList item_footer={this.state.footer} />}
-
-                </div>
-
+                    <Routes>
+                        <Route exact path='/' element={<Navigate to='/users' />} />
+                        <Route exact path='/projects' element={<ProjectList projects={this.state.projects} />} />
+                        <Route exact path='/todos' element={<TodoList todos={this.state.todos} />} />
+                        <Route path='/users'>
+                            <Route index element={<UserList users={this.state.users} />} />
+                            <Route path=':userId' element={<UserProjectList projects={this.state.projects} />} />
+                        </Route>
+                        <Route exact path='*' element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
             </div>
+
+
+
+                 {/*<div className='content'>*/}
+                 {/*    {<UserList users={this.state.users} />}*/}
+                 {/*</div>*/}
+
+                 <div className='panel-footer'>
+                     {<FooterList item_footer={this.state.footer} />}
+               </div>
+             </div>
         )
     }
 }
