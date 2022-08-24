dd6f99a4f932fb9a546cf1c5c0d7d2d1e9f49905
Roman <rombos1978@gmail.com>
2022-08-24 21:43:16 +0300

    6@2

--- backend/TODO/admin.py
+++ backend/TODO/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
 
-
-admin.site.register(User)
+@admin.register(User)
+class CustomUserModelAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email',)
+    list_display_links = ('username',)
+    search_fields = ('username', 'email',)
--- backend/TODO/models.py
+++ backend/TODO/models.py
@@ -18,3 +18,4 @@ class User(AbstractUser):
 
 """Superuser - login: django
 password: geekbrains"""
+
